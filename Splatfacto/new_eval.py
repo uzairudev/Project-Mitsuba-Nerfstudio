461a011e9266c8a507746cbde00c63ccda8bbd3b
UZAIRU ABUBAKAR <uzairukwaire@gmail.com>
2024-08-03 04:45:19 +0200

    Files complete

---
+++ Splatfacto/new_eval.py
@@ -0,0 +1,38 @@
+import torch
+from pytorch_msssim import SSIM
+from torchmetrics.image import PeakSignalNoiseRatio
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+import matplotlib.pyplot as plt
+
+# Used in the model to save the predicted image to a tensor file.
+# torch.save(predicted_rgb, 'first_sample.pth')
+# torch.save(predicted_rgb, 'second_sample.pth')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+first_sample = torch.load('first_sample.pth', weights_only=True).to(device)
+second_sample = torch.load('second_sample.pth', weights_only=True).to(device)
+# second_sample = torch.load('first_sample.pth', weights_only=True).to(device)
+
+psnr = PeakSignalNoiseRatio(data_range=1.0).to(device)
+ssim = SSIM(data_range=1.0, size_average=True, channel=3)
+lpips = LearnedPerceptualImagePatchSimilarity(normalize=True).to(device)
+
+psnr_value = psnr(first_sample, second_sample)
+ssim_value = ssim(first_sample, second_sample)
+lpips_value = lpips(first_sample, second_sample)
+
+metrics = ['PSNR', 'SSIM', 'LPIPS']
+values = [psnr_value, ssim_value, lpips_value]
+
+plt.title('Comparison of PSNR, SSIM, and LPIPS Metrics')
+plt.xlabel('Metrics')
+plt.ylabel('Values')
+
+from pprint import pprint
+
+for i, value in enumerate(values):
+    plt.text(i, value + 0.05, f'{value:.2f}', ha='center')
+    pprint(value)
+
+plt.show()
